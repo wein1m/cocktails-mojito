45e1b175fca21cc6b5109d3cf318ccacc7888d76
yota-sz <deyo.yodha@gmail.com>
2025-08-15 21:48:27 +0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+const App = () => {
+  return (
+    <div className="flex-center h-screen">
+    <h1 className="">test</h1>
+    </div>
+  )
+}
+
+export default App
